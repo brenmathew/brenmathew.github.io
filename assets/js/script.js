986c0b5539701f066b4c2af409564b334277b15a
Bren Mathew <brenmathew10@gmail.com>
2024-07-13 01:34:00 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,6 +130,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+
 // Page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
